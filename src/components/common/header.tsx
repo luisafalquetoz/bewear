2caac51a167ee4e341ea95e6d15f04fd6ec38d0a
luisafalquetoz <luisa_fzandonade@hotmail.com>
2025-08-13 16:16:05 -0300

    feat: add product to cart action

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -15,6 +15,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "../ui/sheet";
+import Cart from "./cart";
 
 const Header = () => {
   const { data: session } = authClient.useSession();
@@ -24,7 +25,8 @@ const Header = () => {
       <Link href="/">
         <Image src="/logo.svg" alt="BEWEAR" width={100} height={26.14} />
       </Link>
-      <div className="flex items-center">
+      <div className="flex items-center gap-2">
+        <Cart />
         <Sheet>
           <SheetTrigger asChild>
             <Button variant="outline" size="icon">

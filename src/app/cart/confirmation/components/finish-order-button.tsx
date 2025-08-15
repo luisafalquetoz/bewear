39e41eb67e828bf18ecd483d0f83952446eeef54
luisafalquetoz <luisa_fzandonade@hotmail.com>
2025-08-14 22:06:55 -0300

    feat: ensure order can be created

---
+++ src/app/cart/confirmation/components/finish-order-button.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useFinishOrder } from "@/hooks/mutations/use-finish-order";
+
+const FinishOrderButton = () => {
+  const [dialogIsOpen, setDialogIsOpen] = useState(true);
+  const FinishOrderMutation = useFinishOrder();
+  return (
+    <>
+      <Button
+        className="w-full rounded-full"
+        size="lg"
+        onClick={() => FinishOrderMutation.mutate()}
+        disabled={FinishOrderMutation.isPending}
+      >
+        {FinishOrderMutation.isPending && (
+          <Loader2 className="h-4 w-4 animate-spin" />
+        )}
+        Finalizar compra
+      </Button>
+      <Dialog open={dialogIsOpen} onOpenChange={setDialogIsOpen}>
+        <DialogContent className="text-center">
+          <Image
+            src="/illustration.svg"
+            alt="Success"
+            width={300}
+            height={300}
+            className="mx-auto"
+          />
+          <DialogTitle className="mt-4 text-2xl">Pedido efetuado!</DialogTitle>
+          <DialogDescription className="font-medium">
+            Seu pedido foi efetuado com sucesso. Você pode acompanhar o status
+            na seção de “Meus Pedidos”.
+          </DialogDescription>
+          <DialogFooter>
+            <Button className="rounded-full" size="lg">
+              Ver meu pedido
+            </Button>
+            <Button className="rounded-full" size="lg" variant="outline">
+              Página inicial
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
+
+export default FinishOrderButton;
---
+++ src/hooks/mutations/use-finish-order.ts
@@ -0,0 +1,18 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { finishOrder } from "@/actions/finish-order";
+
+export const getUseFinishOrderMutationKey = () => ["finish-order"];
+
+export const useFinishOrder = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationKey: getUseFinishOrderMutationKey(),
+    mutationFn: async () => {
+      await finishOrder();
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["cart"] });
+    },
+  });
+};
